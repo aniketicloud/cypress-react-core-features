68a7597e885cc522bfdbf996fefcdcd259945155
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-02-27 00:10:46 +0530

    get() vs find()

--- cypress/e2e/basics.cy.js
+++ cypress/e2e/basics.cy.js
@@ -3,11 +3,14 @@
 describe("tasks page", () => {
   it("should render the main page", () => {
     cy.visit("http://localhost:5173/");
-    cy.get(".main-header img");
+    cy.get(".main-header").find("img");
+    // ! Note: do not use get chaining. it starts as separate get commands. 
+    // ! e.g. do not use -> cy.get(".main-header").get("img")
+    // ! Note: do not use find diretly. always chain find after get.
   });
   it("should display the page title", () => {
     cy.visit("http://localhost:5173/");
-    cy.get("h1").should("have.length", 1)
-    cy.get("h1").contains("My Cypress Course Tasks")
-  })
+    cy.get("h1").should("have.length", 1);
+    cy.get("h1").contains("My Cypress Course Tasks");
+  });
 });
